f50cdb62aba68dfb04b8b45c48bf816e346356fc
Wouter Geraedts <git@woutergeraedts.nl>
2016-11-08 23:12:41 +0100

    Rewrite to use cheerio

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const request = require('request');
-const jsdom = require('jsdom');
+const cheerio = require('cheerio');
 const _ = require('lodash');
 
 const baseUrl = "https://www.coop.nl/odoornschippers";
@@ -14,43 +14,45 @@ fs.readFileSync('cookies', 'utf8').split('; ').forEach((x) => {
 	jar.setCookie(x, baseUrl);
 });
 
+const byClass = (node, name) => {
+	return node.getElementsByClassName(name);
+}
+
 const getBasket = (f) => {
 	request.get({url: baseUrl+'/winkelmand', jar: jar}, (err, resp, body) => {
-		jsdom.env(body, [jQueryUrl], (err, window) => {
-			let result = [];
-			window.$('.productItem').each((i, pi) => {
-				const amount = parseInt(pi.querySelector('input').getAttribute('value'));
-				const totalPrice = parseFloat(pi.querySelector('.price').textContent.replace(',', '.'));
-				result.push({
-					'id': pi.querySelector('.stepper').getAttribute('data-basketstepper-sku'),
-					'title': pi.querySelector('.productTitle').textContent,
-					amount,
-					totalPrice,
-					'price': totalPrice / amount
-				});
+		let $ = cheerio.load(body);
+		let result = [];
+		$('.productItem').each((i, pi) => {
+			const amount = parseInt($(pi).find('input').attr('value'));
+			const totalPrice = parseFloat($(pi).find('.price').text().replace(',', '.'));
+			result.push({
+				'id': $(pi).find('.stepper').attr('data-basketstepper-sku'),
+				'title': $(pi).find('.productTitle').text(),
+				amount,
+				totalPrice,
+				'price': totalPrice / amount
 			});
-			f(result);
 		});
+		f(result);
 	});
 };
 
 const search = (query, f) => {
 	request.get({url: baseUrl+'/zoeken?SearchTerm='+encodeURI(query), jar:jar}, (err, resp, body) => {
-		jsdom.env(body, [jQueryUrl], (err, window) => {
-			let result = [];
-			window.$('.listItem').each((i, pi) => {
-				const amount = parseInt(pi.querySelector('input').getAttribute('value'));
-				const price = parseFloat(pi.querySelector('.price').textContent.replace(',', '.'));
-				result.push({
-					'id': pi.querySelector('a').getAttribute('href').split('/').pop(),
-					'title': pi.querySelector('.productTitle').textContent.trim(),
-					amount,
-					'totalPrice': price * amount,
-					'price': price
-				});
+		let $ = cheerio.load(body);
+		let result = [];
+		$('.listItem').each((i, pi) => {
+			const amount = parseInt($(pi).find('input').attr('value'));
+			const price = parseFloat($(pi).find('.price').text().replace(',', '.'));
+			result.push({
+				'id': $(pi).find('a').attr('href').split('/').pop(),
+				'title': $(pi).find('.productTitle').text().trim(),
+				amount,
+				'totalPrice': price * amount,
+				'price': price
 			});
-			f(result);
 		});
+		f(result);
 	});
 };
 
